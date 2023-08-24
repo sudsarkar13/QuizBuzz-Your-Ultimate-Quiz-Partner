f0bb38f2158f73442b922237ba3ef2e4d2a0c217
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-08-24 11:08:28 +0530

    Changing to only javaScript

--- src/js/script.js
+++ src/js/script.js
@@ -12,8 +12,75 @@ loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
 });
 
-// Login & Signup validation
-document.getElementById("SignIn")
+// Error, Login & Signup validation
+
+// function seterror(id, error) {
+//     element = document.getElementById(id);
+//     element.getElementsByClassName('validation')[0].innerHTML = error;
+// }
+
+// function validateForm() {
+//     var returnval = true;
+
+//     // Name
+//     var name = document.forms['SignUp']['name'].value;
+//     if (name.length < 6) {
+//         seterror('name', 'Name must be at least 6 characters.');
+//         returnval = false;
+//     }
+// }
+
+// Functions for validation
+
+
+// Shubham Tiwari edits for validation
+
+const formSignUp = document.getElementById('sign-up');
+const inputBox = formSignUp.querySelectorAll('.input-box');
+
+formSignUp.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log(formSignUp);
+    console.log(inputBox);
+
+    const inputName = inputBox[0].querySelector('#name').value;
+    const inputEmail = inputBox[1].querySelector('#email').value;
+    const inputPhone = inputBox[2].querySelector('#phone').value;
+    const inputPassword = inputBox[3].querySelector('#password').value;
+    const inputConfirmPassword = inputBox[4].querySelector('#cnfpassword').value;
+
+    console.log(inputName, inputPhone, inputPassword, inputConfirmPassword);
+
+    if(inputName.length < 6)
+        console.log("Name is too short")
+
+    if(!validateEmail(inputEmail))
+        console.log("Invalid Email");
+
+    if(!validatePhone(inputPhone))
+        console.log("Invalid Phone");
+
+    if(inputConfirmPassword != inputPassword){
+        console.log("password does'nt match");
+    }
+})
+
+// This will validate your email 
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+function validateEmail(email) {
+  return emailRegex.test(email);
+}
+
+
+const phoneRegex = /^[6-9]\d{9}$/;
+
+function validatePhone(phone) {
+  return phoneRegex.test(phone);
+}
+
+
 
 // footer year
 VarDate = new Date();
