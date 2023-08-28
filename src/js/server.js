e9bea158bdd6810778a8aeb130d3487534f0306f
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-08-28 10:28:20 +0530

    commit modules

--- src/js/server.js
+++ src/js/server.js
@@ -1,22 +1,59 @@
-const express = require ('express');
-const app = express ();
-const mongoose = require ('mongoose');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path'); 
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://sudsarkar13:bP8ctucxDgPKVQLk@cluster0.wysx6kz.mongodb.net/testDB")
+mongoose.connect("mongodb+srv://sudsarkar13:bP8ctucxDgPKVQLk@cluster0.wysx6kz.mongodb.net/testDB", { useNewUrlParser: true, useUnifiedTopology: true, })
+
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 // Create a data schema
-const testSchema = {
-    title: String,
-    content: String
-}
-
-app.get('/', function(req, res){
-    res.send("express server is up and running");
+const userSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    phoneNumber: String,
+    password: String,
 });
 
-app.listen(3000, function(){
+module.exports = mongoose.model('User', userSchema);
+
+// app.get('/', function (req, res) {
+//     const indexPath = path.join(__dirname, '/index.html');
+// });
+
+// Set up static file serving for CSS, JS, and images
+app.use(express.static(path.join(__dirname, '/index.html')));
+
+//post
+app.post('/register', async (req, res) => {
+    try {
+        const { fullName, email, phoneNumber, password } = req.body;
+
+        // Create a new user instance using the User model
+        const newUser = new User({
+            fullName,
+            email,
+            phoneNumber,
+            password,
+        });
+
+        // Save the new user to the database
+        await newUser.save();
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
